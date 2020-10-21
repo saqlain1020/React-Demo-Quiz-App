069620b5d5ba0ec9db2910a8394c0af2574b5936
= <=>
2020-10-21 14:55:51 +0500

    quiz create

--- src/Components/QuizCreate/QuizCreate.js
+++ src/Components/QuizCreate/QuizCreate.js
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 class QuizCreate extends Component {
   state = {
     noOfQs: 0,
+    title: "",
   };
   componentDidMount() {
-console.log(this.state.noOfQs)    let num = Number(prompt("Enter Number Of Questions"));
+    let num = Number(prompt("Enter Number Of Questions"));
     if (isNaN(num) || num < 1) this.componentDidMount();
     else
       this.setState(
@@ -14,11 +15,22 @@ console.log(this.state.noOfQs)    let num = Number(prompt("Enter Number Of Quest
         },
         () => {
           console.log(this.state.noOfQs);
+          let title = prompt("Enter Quiz Title");
+          this.setState({
+              title: title
+          })
         }
       );
   }
 
-  render = () => <div id="quizCreate"></div>;
+  render = () => <div id="quizCreate">
+      <input readonly='true' type="text" defaultValue={this.state.title}/>
+      <textarea type="text" placeholder={this.state.question}/>
+      <input type="text" placeholder="Question 1" style={{background: 'green',color='white'}}/>
+      <input type="text" placeholder="Question 2"/>
+      <input type="text" placeholder="Question 3"/>
+      <input type="text" placeholder="Question 4"/>
+  </div>;
 }
 
 export default QuizCreate;
